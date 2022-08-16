84a1edf747a133d467b2c65fe2898c4b317e30bd
Diego Cintra <diegoffcintra@gmail.com>
2022-08-16 10:36:11 -0300

    alterar deploy

--- release-tasks.sh
+++ release-tasks.sh
@@ -11,9 +11,6 @@ trap notify ERR
 # -E ensures that ERR traps get inherited by functions, command substitutions, and subshell environments.
 set -Eex
 
-# abort release if deploy status equals "blocked"
-[[ $DEPLOY_STATUS = "blocked" ]] && echo "Deploy blocked" && exit 1
-
 # runs migration for Postgres and boots the app to check there are no errors
 STATEMENT_TIMEOUT=4500000 bundle exec rails app_initializer:setup
 bundle exec rake fastly:update_configs
